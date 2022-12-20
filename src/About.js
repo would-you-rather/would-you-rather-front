0013f1f2aaa33257e84fc28f837528dfbfc7c6a5
DEIOSHA SPARKS <DEIOSHASPARKS13@GMAIL.COM>
2022-12-20 10:35:29 -0800

    created files for auth0

---
+++ src/About.js
@@ -0,0 +1,17 @@
+import { Component } from "react";
+
+class About extends Component {
+
+  render() {
+    return (
+      <>
+      <h1>About The Devs</h1>
+      <p>My name is Deiosha</p>
+      <p>My name is Ashwini</p>
+      <p>My name is Adrian</p>
+      </>      
+    )
+  }
+};
+
+export default About;
---
+++ src/auth/makeAuthenticatedRequest.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+async function makeAuthenticatedRequest(url, method, data) {
+
+  let request = {
+    url: url, 
+    method: method,
+    data: data,
+    headers: {
+      'Authorization': 'Bearer ${token}'
+    }
+  }
+
+}
