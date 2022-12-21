7fcdff21bd64313895aadd3eed18b07412822eb2
Ash Uppal <ashuppal@gmail.com>
2022-12-20 19:07:56 -0800

    post request working

--- src/GameSelection.js
+++ src/GameSelection.js
@@ -1,41 +1,100 @@
 import React from 'react';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, Form } from 'react-bootstrap/';
+import {withAuth0} from '@auth0/auth0-react';
+import axios from 'axios';
+
 
 class GameSelection extends React.Component {
     constructor() {
+        
         super();
         this.state = {
+          roomId: '',
         }
     }
+
+    request = async() => {
+        let res =  await this.props.auth0.getIdTokenClaims();
+        let token = res._raw;
+        console.log(token);
+    
+        let request = {
+          method: 'GET',
+          url: 'http://localhost:3002/rooms',
+          headers: {
+            Authorization:`Bearer ${token}`
+          }
+        }
+    
+        let response = await axios(request);
+        console.log(response.data);
+      }
+    
+    createRoom = async() => {
+        let res =  await this.props.auth0.getIdTokenClaims();
+        let token = res._raw;
+        console.log(token);
+
+        let request = {
+            method: 'POST',
+            url:'http://localhost:3002/rooms',
+            headers: {
+                authorization:`Bearer ${token}`,
+            },
+            data: {
+                "owner": this.props.auth0.user.email,
+                "questionListId": "Fun"
+            }
+
+        };
+
+        let response = await axios(request);
+        console.log(response.data);
+
+        this.setState({
+            roomId: response.data._id
+        });
+
+    }
+
+
+
+    
     render() {
 
         return (
+        
             <>
+               
         <h1>Welcome to This or That</h1>
-        <Form>
-        <Form.Label>Choose a theme</Form.Label>
+
+        { (!this.state.roomId) && <>
+            <Form>
+                <Form.Label>Choose a theme</Form.Label>
                 <Form.Select onChange = {this.handleChange} as = 'select'>
-                    <option>Fun</option>
-                    
+                            <option>Fun</option>
+                            
                 </Form.Select>
-        </Form>
-        <div>
-        <Button>Create Room</Button>
-        
-        <Button>Start Game</Button>
+            </Form>
+            <div>
+                <Button
+                    onClick = {this.createRoom}>Create Room</Button>
+            </div>
+            </>
+        }
 
-        </div>
-        
-
-        <div>
-            <p>.... people have joined.</p>
-        </div>
-        <div>
-            <p>Share the following link with your friends.</p>
-            <link></link>
-            <Button>Copy</Button>
-        </div>
+        {this.state.roomId &&
+            <div>
+                <Button>Start Game</Button>
+                <p>Share the following link with your friends.</p>
+                <input 
+                    readOnly= {true} 
+                    type="text" 
+                    value = {window.location.href + "#/?room=" + this.state.roomId} />
+                <Button onClick={() => {navigator.clipboard.writeText(window.location.href + "#/?room=" + this.state.roomId)}}>Copy</Button>         
+            </div>
+        }
 
         
 
@@ -44,4 +103,4 @@ class GameSelection extends React.Component {
     }
 }
     
-export default GameSelection;
+export default withAuth0(GameSelection);
